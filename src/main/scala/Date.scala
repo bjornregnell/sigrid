f90c35de97513ea9297c89ebef4a13768eb360ce
bjornregnell <bjorn.regnell@cs.lth.se>
2019-11-19 21:49:42 +0100

    add factory and minusHours

--- src/main/scala/Date.scala
+++ src/main/scala/Date.scala
@@ -5,18 +5,23 @@ case class Date(year: Int, month: Int, dayOfMonth: Int,
                 hour: Int, minute: Int, second: Int
                 ) extends Ordered[Date] {
 
-  val calendar = LocalDateTime.of(year, month, dayOfMonth, hour, minute, second)
+  val dateTime = LocalDateTime.of(year, month, dayOfMonth, hour, minute, second)
 
-  def toFormat(format: String): String = calendar.format(DateTimeFormatter.ofPattern(format))
+  def toFormat(format: String): String = dateTime.format(DateTimeFormatter.ofPattern(format))
 
   lazy val show: String = toFormat("yyyy-MM-dd HH:mm:ss")
 
-  def compare(other: Date): Int = calendar.compareTo(other.calendar)
+  def compare(other: Date): Int = dateTime.compareTo(other.dateTime)
+
+  def minusHours(h: Long): Date = Date(dateTime.minusHours(h))
+  
 }
 
 object Date {
-  def now(): Date = {
-    import LocalDateTime.{now => j} 
+  def now(): Date = apply(LocalDateTime.now)
+
+  def apply(dateTime: LocalDateTime): Date = {
+    val j = dateTime
     Date(j.getYear, j.getMonthValue, j.getDayOfMonth, j.getHour, j.getMinute, j.getSecond)
   }
 }
